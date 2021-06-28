33fef41b343630bc9295fec4b085530dddeea347
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-06-28 13:06:47 -0300

    preview of img

--- src/screens/TransferScreen.js
+++ src/screens/TransferScreen.js
@@ -8,6 +8,7 @@ import { PaymentContainer }from '../components/PaymentContainer'
 import { payWithBank } from '../redux/actions/paymentActions'
 import { firstData, secondData } from '../helpers/transferData'
 import { NotFound } from './NotFound'
+import { useState } from 'react'
 
 const useStyles = makeStyles((theme)=>({
     eachText:{
@@ -17,22 +18,54 @@ const useStyles = makeStyles((theme)=>({
     button:{
         width:'80%',
         marginTop:'2rem',
+        display:'block',
+        marginBottom:'2rem',
         [theme.breakpoints.down("sm")]: { 
             width:'100%',
             marginBottom: '1rem'
         }
+
+    },
+    eachImg:{
+        width:'5rem',
+        height:'5rem',
+        marginBottom:'0.3rem'
+    },
+    eachImgContainer:{
+        display: 'flex',
+        flexDirection:'column',
+        alignItems:'center',
+        marginRight:'1rem'
+    },
+    imgContainer:{
+        display:'flex'
+    },
+    btnDelete:{
+        height:'1.3rem',
+        marginBottom:'1rem'
 
     }
 }))
 export const TransferScreen = () => {
     const classes = useStyles()
     const dispatch = useDispatch()
+    const [images, setImages] = useState([])
     const urlData = useSelector(state=>state.url.urlData)
     const handleOnClick = () =>{
         document.querySelector('#fileSelector').click()
     }
     const handleFileChange = (e) =>{
-        dispatch(payWithBank(e.target.files[0], urlData.enrcyptedId))
+        
+        if(e.target.files.length === 0 ){
+            return null
+        }
+        const files = Array.from(e.target.files)
+        let newArray = []
+        files.map(element => {
+            newArray.push(URL.createObjectURL(element))
+        })
+        setImages([...images,...newArray])
+        //dispatch(payWithBank(e.target.files[0], urlData.enrcyptedId))
     }
     return (
         <>
@@ -44,8 +77,25 @@ export const TransferScreen = () => {
                     <Typography className={classes.eachText} >CBU : {urlData.bankType===0?(firstData.cbu):(secondData.cbu)}</Typography>
                     <Typography className={classes.eachText} >Alias : {urlData.bankType===0?(firstData.alias):(secondData.alias)}</Typography>
                     <Typography className={classes.eachText} >Titular : {urlData.bankType===0?(firstData.owner):(secondData.owner)}</Typography>
-                    <input type='file' style={{display:'none'}} onChange={handleFileChange} id='fileSelector' name='file'/>
+                    <input type='file' multiple style={{display:'none'}} onChange={handleFileChange} id='fileSelector' name='file'/>
                     <Button onClick={handleOnClick} className={classes.button} variant='contained' color='primary'>Cargar comprobante de pago</Button>
+                    {
+                        images&&(
+                            <div className={classes.imgContainer}>
+                                {
+                                    
+                                    images.map((eachImage)=>(
+                                        <div key={eachImage} className={classes.eachImgContainer}>
+                                            <img src={eachImage} className={classes.eachImg}/>
+                                            <Button className={classes.btnDelete} color='primary' variant='contained' size='small'>BORRAR</Button>
+                                        </div>
+                                    ))
+                                }
+                                
+                                
+                            </div>
+                        )
+                    }
                 </PaymentContainer>
             ):
             (
