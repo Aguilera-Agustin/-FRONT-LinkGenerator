0d028ec91dd140f3a726e7fb5d67135ef31c8030
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-08-15 09:31:20 -0300

    more data in transfer

--- src/screens/TransferScreen.js
+++ src/screens/TransferScreen.js
@@ -34,6 +34,8 @@ export const TransferScreen = ({type}) => {
     const urlData = useSelector(state=>state.url.urlData)
     const [bank, setBank] = useState('')
     const {acc, alias, num, transferData, type:transferType, bank:usdBank} = getTransferData(urlData.business_type, type, bank)
+    const myData = getTransferData(urlData.business_type, type, bank)
+    console.log(myData)
     
     const changeBank = (e) =>{
         setBank(e.target.value.toLowerCase())
@@ -46,10 +48,13 @@ export const TransferScreen = ({type}) => {
                 <PaymentContainer button available={isDateAvailable(urlData.createdAt, urlData.duration)}>
                     <Typography variant="h5" align='center' color="initial" style={{marginTop:'1rem'}}>Datos</Typography>
                     <TransferSelector type={type} changeBank={changeBank} bank={bank}/>
+                    <Typography className={classes.eachText} >Titular : {urlData.business_type===0?('Luis Fernando de Jesus Nuñez'):('Supersistemasweb SRL')}  </Typography>
                     <Typography className={classes.eachText} >Banco : {type==='usd'?(usdBank) : (bank.toUpperCase() || 'MACRO')}  </Typography>
                     <Typography className={classes.eachText} >Tipo : {acc}  </Typography>
                     <Typography className={classes.eachText} >{transferType} : {transferData}</Typography>
                     <Typography className={classes.eachText} >Alias : {alias}</Typography>
+                    <Typography className={classes.eachText} >Cuit : {urlData.business_type===0?('20-26110188-3'):('33-71469665-9')}</Typography>
+                    <Typography className={classes.eachText} >Razón social : {urlData.business_type}</Typography>
                     {
                         num&&(
                             <Typography className={classes.eachText} >Número de cuenta : {num}</Typography>
